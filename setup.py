07e5888891cfd30cc5d8f6665af4c971c4db649e
Dragos Velicanu <dragosvelicanu@gmail.com>
2024-01-17 15:58:15 -0500

    try ci again with set x (#4)
    * try ci again with set x
    
    * change ci order

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open(os.path.join(SCRIPT_DIR, "README.md")).read()
 
 setup(
     name="tosql",
-    version="0.0.4",
+    version="0.0.5",
     description="tosql - pipe data to sql queries",
     long_description=README,
     long_description_content_type="text/markdown",
